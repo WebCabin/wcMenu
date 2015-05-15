1c84d2ae8f3d258c7e4958194e9a2fa925a6a643
lochemage <lochemage@webcabin.org>
2015-05-15 02:18:10 -0700

    Fixed up documentation.

--- demo.js
+++ demo.js
@@ -10,74 +10,70 @@ $(window).ready(function() {
     version: 'v1.0.0'
   });
 
-  if (myMenu) {
+  // A simple menu option.
+  myMenu.addOption('File', 'Option', {
+    hotkeys: 'Alt+O,Shift+O',
+    icon: "fa fa-file-o fa-lg",
+    description: "Open a new file.",
+    toolbarIndex: -1,
+    onActivated: function(data) {
+      alert('File -> Option activated!');
+    }
+  });
 
-    // A simple menu option.
-    myMenu.addOption('File', 'Option', {
-      hotkeys: 'Alt+O,Shift+O',
-      icon: "fa fa-file-o fa-lg",
-      description: "Open a new file.",
-      toolbarIndex: -1,
-      onActivated: function(data) {
-        alert('File -> Option activated!');
+  // Put a spacer right after the last 'File' menu item button in the toolbar.
+  myMenu.addToolbarSpacer('File', 'Option');
+
+  // A more dynamic set of menu options.
+  myMenu.addOption('Toggle', 'Option Enabled', {
+    hotkeys: 'E',
+    description: 'Toggle enabled status of the next option.',
+    toolbarIndex: -1,
+    display: function(data) {
+      if (data.isEnabled) {
+        return "Option On";
+      } else {
+        return "Option Off";
       }
-    });
-
-    // Put a spacer right after the last 'File' menu item button in the toolbar.
-    myMenu.addToolbarSpacer('File', 'Option');
-
-    // A more dynamic set of menu options.
-    myMenu.addOption('Toggle', 'Option Enabled', {
-      hotkeys: 'E',
-      description: 'Toggle enabled status of the next option.',
-      toolbarIndex: -1,
-      display: function(data) {
-        if (data.isEnabled) {
-          return "Option On";
-        } else {
-          return "Option Off";
-        }
-      },
-      icon: function(data) {
-        if (data.isEnabled) {
-          return "fa fa-toggle-on fa-lg";
-        } else {
-          return "fa fa-toggle-off fa-lg";
-        }
-      },
-      onActivated: function(data) {
-        data.isEnabled = !data.isEnabled;
+    },
+    icon: function(data) {
+      if (data.isEnabled) {
+        return "fa fa-toggle-on fa-lg";
+      } else {
+        return "fa fa-toggle-off fa-lg";
       }
-    });
+    },
+    onActivated: function(data) {
+      data.isEnabled = !data.isEnabled;
+    }
+  });
 
-    myMenu.addSpacer('Toggle', 'Option Enabled');
+  myMenu.addSpacer('Toggle', 'Option Enabled');
 
-    myMenu.addOption('Toggle', 'Button', {
-      hotkeys: 'B',
-      icon: "fa fa-crosshairs fa-lg",
-      toolbarIndex: -1,
-      description: function(data) {
-        if (data.isEnabled) {
-          return "This option is enabled!";
-        } else {
-          return "This option is disabled!";
-        }
-      },
-      condition: function(data) {
-        return data.isEnabled;
-      },
-      onActivated: function(data) {
-        alert("Toggle -> Button activated!");
+  myMenu.addOption('Toggle', 'Button', {
+    hotkeys: 'B',
+    icon: "fa fa-crosshairs fa-lg",
+    toolbarIndex: -1,
+    description: function(data) {
+      if (data.isEnabled) {
+        return "This option is enabled!";
+      } else {
+        return "This option is disabled!";
       }
-    });
+    },
+    condition: function(data) {
+      return data.isEnabled;
+    },
+    onActivated: function(data) {
+      alert("Toggle -> Button activated!");
+    }
+  });
 
+  // myMenu.removeOption('Toggle', 'Option Enabled');
 
-    // myMenu.removeOption('Toggle', 'Option Enabled');
-
-    // for (var i = 0; i < 100; ++i) {
-    //   myMenu.addOption('Toggle' + i, 'Test' + i, {
-    //     toolbarIndex: -1
-    //   });
-    // }
-  }
+  // for (var i = 0; i < 100; ++i) {
+  //   myMenu.addOption('Toggle' + i, 'Test' + i, {
+  //     toolbarIndex: -1
+  //   });
+  // }
 });
